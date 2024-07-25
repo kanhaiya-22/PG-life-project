5bfbabff357200985727432b36b930014c505e5e
kanhaiya-22 <kanhaiyamittal8755@gmail.com>
2024-07-25 14:50:31 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in    Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <br>
+            © 2024 Copyright PG Life
+            <br>
+            Made by:
+        © <a href="https://www.linkedin.com/in/kanhaiya-mittal-632423251/">Kanhaiya Mittal</a>
+        <br>
+        <a href="https://www.ietlucknow.ac.in/">IET LUCKNOW</a>
+        <br>
+        <a href="index.php">
+            Home
+            <br>
+        </a>
+        <a href="#">Back to top</a>
+       
+        </div>
+     </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
